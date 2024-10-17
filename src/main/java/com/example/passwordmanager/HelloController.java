946bd8fda0185b708cad93be6fe48a5fe03b70f5
0SAS0 <szymon.niewiadomski@teb.edu.pl>
2024-10-17 19:49:13 +0200

    v1.0

---
+++ src/main/java/com/example/passwordmanager/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.passwordmanager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    protected void CpPassword() {
+
+    }
+}
