65ff2893fe39d2977ae458ba18b9bfd74fd4542e
0SAS0 <szymon.niewiadomski@teb.edu.pl>
2024-10-20 21:00:12 +0200

    v1.4

--- src/main/java/com/example/passwordmanager/JsonPassword.java
+++ src/main/java/com/example/passwordmanager/JsonPassword.java
@@ -2,9 +2,22 @@ package com.example.passwordmanager;
 
 import org.json.simple.JSONObject;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class JsonPassword {
-    JSONObject obj = new JSONObject();
-    public JsonPassword() {
-        obj.put("type", acType)
+    static String randomPassword = PasswordGenerator.generatePassword();
+    public static void passwd(String acType) {
+        JSONObject obj = new JSONObject();
+        obj.put("Type", acType);
+        obj.put("Password", randomPassword);
+        try {
+            FileWriter file = new FileWriter("Password.json");
+            file.write(obj.toJSONString());
+            file.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Json file created");
     }
 }
